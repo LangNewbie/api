444a46de77e8a0dd4461128b16f04ec751170153
LangNewbie <144513851+LangNewbie@users.noreply.github.com>
2023-09-09 13:05:43 +0700

    Update nodemailer.js

--- library/nodemailer.js
+++ library/nodemailer.js
@@ -1,19 +1,26 @@
 const nodemailer = require("nodemailer");
 const { passEmailApp, myEmail } = require("./settings");
+const axios = require("axios");
 
 async function sendEmail(dataEmail, req, res, path1, path2) {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        requireTLS: true,
-        auth: {
-          user: myEmail, 
-          pass: passEmailApp, 
-        },
-        from: myEmail
-    });
-    transporter.sendMail(dataEmail, (err, info) => {
+    const data = JSON.stringify({
+    "Messages": [{
+      "From": {"Email": "botzlang1@gmail.com", "Name": "RestAPI - By Lang"},
+      "To": dataEmail.to,
+      "Subject": dataEmail.subject,
+      "Html": dataEmail.html,
+    }]
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.mailjet.com/v3.1/send',
+    data: data,
+    headers: {'Content-Type': 'application/json'},
+    auth: {username: 'b4c5c0fb4593df6b24e72cc24e9f60cf', password: '1a46d9be174b4065b4732e58d6da12ad'},
+  };
+
+  axios(config, (err, response) => {
         if (err) {
             console.log(err)
             req.flash('error_msg', 'Something Wrong');
